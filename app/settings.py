5b66376eb763a01990fdefbdff4c33d1f108c321
Protsenko <a.protsenko@agora.ru>
2023-06-29 13:57:56 +1000

    fixes config

--- app/settings.py
+++ app/settings.py
@@ -15,18 +15,17 @@ AppConfigType = TypedDict("AppConfigType", {
     "sync": List[SyncConfig],
 })
 
-config: AppConfigType = get_config("config.yaml")
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+DB_PATH = os.path.join(BASE_PATH, "default.sqlite")
+
+config_path = os.environ.get("YA_DISK_CONFIG") or os.path.join(BASE_PATH, "config.yaml")
+config: AppConfigType = get_config(config_path)
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-
-
-BASE_PATH = os.path.dirname(os.path.dirname(__file__))
-
-DB_PATH = os.path.join(BASE_PATH, "default.sqlite")
 
 QUEUE_SIZE = 100
 READ_FILE_CHUNK_SIZE = 5 * 1024 * 1024
